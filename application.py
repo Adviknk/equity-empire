1b50af1bf42f1a1ceadc98a83cacde0711a6f2f6
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-06-02 11:44:15 -0400

    commit

--- application.py
+++ application.py
@@ -1,26 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from helper import *
 
 app = Flask(__name__)
 
+signedIn = False
+
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
 
 
-@app.route("/add")
-def add():
-    return "testing extension"
+@app.route("/account")
+def account():
+    if signedIn:
+        return render_template('account.html')
+    else:
+        return redirect('/login')
 
 
-@app.route("/signup")
-def sign_in():
-    return render_template('sign-up.html')
-
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def log_in():
-    return render_template('log-in.html')
+    if request.method == "GET":
+        return render_template("log-in.html")
+
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(email)
+        if email == '':
+            return render_template("login-error.html", error='email is invalid')
+        if password == '':
+            return render_template("login-error.html", error='password is invalid')
+
+        signedIn = True
+
+        return render_template("account.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "GET":
+        return render_template("sign-up.html")
+
+    if request.method == "POST":
+        first = request.form.get('first')
+        last = request.form.get('last')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if first == '':
+            return render_template("signup-error.html", error='first name is invalid')
+        if last == '':
+            return render_template("signup-error.html", error='last name is invalid')
+        if email == '':
+            return render_template("signup-error.html", error='email is invalid')
+        if password == '':
+            return render_template("signup-error.html", error='password is invalid')
+
+        signedIn = True
+        return render_template("account.html")
+
+
+@app.route("/league/<name>")
+def league(name=''):
+    if (name == ''):
+        return render_template('home.html')
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -1,5 +1,1 @@
-function accountClicked() {
-    window.location.pathname = '/login';
-}
-
 
