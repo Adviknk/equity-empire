26d1e67715d584b4a7af93e11ea07ab1a2b94041
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-06-01 22:33:12 -0400

    commit

--- application.py
+++ application.py
@@ -13,5 +13,15 @@ def add():
     return "testing extension"
 
 
+@app.route("/signup")
+def sign_in():
+    return render_template('sign-up.html')
+
+
+@app.route("/login")
+def log_in():
+    return render_template('log-in.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
