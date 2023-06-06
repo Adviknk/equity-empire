c85185c18fa41ab15c45369a467d66cc8e4f0c60
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-06-06 00:53:07 -0400

    commit

--- helper.py
+++ helper.py
@@ -9,7 +9,6 @@ def load_users():
         table = result.all()
         # lsit of dictionaries for each row
         result_dict = [row._asdict() for row in table]
-        print(result_dict)
 
 
 def authenticate(username, email, password):
