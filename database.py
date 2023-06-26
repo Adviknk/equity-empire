ada0f8f75e8ea0af36374ca66e08eeda6032989f
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-06-26 12:07:43 -0400

    commit

--- database.py
+++ database.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 
 
 db_password = os.getenv("EQUITY_EMPIRE_DATABASE_PASSWORD")
+if db_password is None:
+    print("The environment variable is not set.")
 db_string = "mysql+pymysql://9qnyibjw2viylcgcxr43:" + str(db_password) + \
     "@aws.connect.psdb.cloud/stock-trading-game?charset=utf8mb4"
 
